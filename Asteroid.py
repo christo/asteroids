0a1ecdf14c30c571f7c06146cab7a369832079e0
Chris Mountford <chris@think.net.au>
2020-03-26 12:47:25 +1100

    initial

---
+++ Asteroid.py
@@ -0,0 +1,42 @@
+import pygame
+import random
+
+WHITE = 255, 255, 255
+
+class Asteroid:
+
+    minimum_size = 20
+    
+    def __init__(self, x, y, size=200):
+        self.x = x
+        self.y = y
+        self.size = size
+
+        self.vx = random.random() * 10 - 5
+        self.vy = random.random() * 10 - 5
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, WHITE, (self.x, self.y, self.size, self.size), 5)
+
+    def move(self, width, height):
+        self.x += self.vx
+        self.y += self.vy
+        self.x %= width
+        self.y %= height
+        if abs(self.vx) > 2:
+            self.vx *= 0.999
+        if abs(self.vy) > 2:
+            self.vy *= 0.999
+        
+
+    def explode(self):
+        parts = []
+        self.size /= 2
+        if self.size > self.minimum_size:
+            for p in range(3):
+                parts.append(Asteroid(self.x, self.y, self.size))
+        return parts
+
+    def inside(self, pos):
+        return pygame.Rect(self.x, self.y, self.size, self.size).collidepoint(pos)
+    
---
+++ asteroids.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3 
+
+import sys
+import pygame
+import random
+
+from Asteroid import Asteroid
+
+BLACK = 0, 0, 0
+
+WHITE = 255, 255, 255
+
+def main():
+
+
+    width = 800
+    height = 600
+    num_asteroids = 10
+
+    pygame.init()
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Asteroids")
+
+
+    font = pygame.font.Font('Planetnv2.ttf', 32)
+    
+
+    clock = pygame.time.Clock()
+
+    # create some asteroids
+    asteroids = []
+    for i in range(num_asteroids):
+        x = random.randint(0, width) 
+        y = random.randint(0, height)
+        a = Asteroid(x, y)
+        asteroids.append(a)
+
+    running = True
+
+    while running:
+        clock.tick(30)
+
+        # check for quit
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # if the left button is pressed
+                if event.button == 1:
+                    for a in asteroids:
+                        if a.inside(event.pos):
+                            parts = a.explode()
+                            if len(parts) > 0:
+                                asteroids.extend(parts)
+                            else:
+                                asteroids.remove(a)
+
+                    
+
+        #update asteroids
+        for a in asteroids:
+            a.move(width, height)
+
+        #draw screen
+        screen.fill(BLACK)
+
+        for a in asteroids:
+            a.draw(screen)
+        
+        text = font.render('Asteroids: %d' % (len(asteroids)), True, WHITE, BLACK)
+        textRect = text.get_rect()
+        textRect.x = 20
+        textRect.y = 20
+        screen.blit(text, textRect) 
+
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    sys.exit(main())
